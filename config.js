6de04176b50911db13d6cf46d30e1e6b3629bcff
David Guttman <david@davidguttman.com>
2017-09-20 16:11:45 -0700

    fix: change db path to project folder (#21)
    closes #21

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var path = require('path')
 
 module.exports = {
   level: {
-    location: process.env.DB_PATH || path.join(__dirname, '../db')
+    location: process.env.DB_PATH || path.join(__dirname, './db')
   },
   authentic: {
     host: process.env.AUTHENTIC_HOST || 'https://ix-id.lincx.la'
